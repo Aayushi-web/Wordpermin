249563c91c3b51eff2b4d16225323217f8f3e944
Aayushi Mishra <mishraaayushi66@gmail.com>
2022-10-05 14:28:26 +0530

    final

--- wpm.java
+++ wpm.java
@@ -26,5 +26,11 @@ System.out.println();
 double start= LocalTime.now().getNano();
 Scanner sc= new Scanner(System.in);
 String typeword= sc.nextLine();
+double end = LocalTime.now().getNano();
+double time=end-start;
+double sec=time/1000000.0;
+int cumchar= typeword.length();
+int wpm=(int) ((((double)cumchar/5)/sec)*60);
+System.out.println("the word per min is:"+wpm+"wpm"+"!!");
     }
 }
