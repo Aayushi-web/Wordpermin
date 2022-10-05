acc6ddb473cba44856c7d40fb885197f49479dcd
Aayushi Mishra <mishraaayushi66@gmail.com>
2022-10-05 13:08:57 +0530

    added countdown

---
+++ wpm.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class wpm{
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("SO ARE YOU READY?!");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("3");
+        TimeUnit.SECONDS.sleep(1);
+System.out.println("2");
+TimeUnit.SECONDS.sleep(1);
+System.out.println("1");
+TimeUnit.SECONDS.sleep(1);
+    }
+}
