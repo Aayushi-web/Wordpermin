46f05dcd9aa903f0376580fdcbbc49a71c9a7e63
Aayushi Mishra <mishraaayushi66@gmail.com>
2022-10-05 13:31:23 +0530

    new commit

--- wpm.java
+++ wpm.java
@@ -1,7 +1,14 @@
+import java.io.ObjectInputFilter.Status;
+import java.time.LocalTime;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class wpm{
+     static String words[]={"Hello,","wello ,","challo ,","let's,go ,","yours ,","drama,","freshly ,",
+    "yellow ,","laptop ,","Aayushi ,","pillow,"
+    };
     public static void main(String[] args) throws InterruptedException {
         System.out.println("SO ARE YOU READY?!");
         TimeUnit.SECONDS.sleep(1);
@@ -11,5 +18,13 @@ System.out.println("2");
 TimeUnit.SECONDS.sleep(1);
 System.out.println("1");
 TimeUnit.SECONDS.sleep(1);
+Random rand= new Random();
+for(int i=0;i<11;i++){
+System.out.print(words[rand.nextInt(10)]+" ");
+}
+System.out.println();
+double start= LocalTime.now().getNano();
+Scanner sc= new Scanner(System.in);
+String typeword= sc.nextLine();
     }
 }
